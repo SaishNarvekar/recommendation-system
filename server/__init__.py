cb92809dda16850a556fcbf2f3e7bec70bd93669
SaishNarvekar <35778345+SaishNarvekar@users.noreply.github.com>
2019-04-13 00:32:13 +0530

    Deleted server-run file and moved all content to init file

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,19 @@
-from server.server_run import app
+from flask import Flask, request
+from flask_cors import CORS
+from server.itinerary import Itinerary
+import json
+
+app = Flask(__name__)
+CORS(app, support_credentials=True)
+app.secret_key = "82bfce4d0166155f1dd8524112584fb1"
+
+@app.route("/")
+def index():
+    return """hello world"""
+
+@app.route('/api/travelType=<travelType>&numberOfDays=<days>&prefence1=<data1>&prefence2=<data2>&prefence3=<data3>&prefence4=<data4>&state=<state>')
+def loc(travelType,days,data1,data2,data3,data4,state):
+    
+    dataList = [data1,data2,data3,data4]
+    itinerary = Itinerary(travelType,days,dataList,state)
+    return json.dumps(itinerary.getItinerary())
